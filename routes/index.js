0eb15027ccf191331b126ce8fb45f49dcfe8785b
rijkvanzanten <rijkvanzanten@me.com>
2017-09-07 17:02:46 -0400

    Render 10 highest voted requests to screen

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const client = require('../directus');
 
 router
   .use('/auth', require('./auth'))
-  .get('/', (req, res) => res.render('index', {user: req.user}));
+  .get('/', sendHome)
+  .all('*', send404);
+
+function sendHome(req, res) {
+  client
+    .getItems('requests', {
+      limit: 10,
+      'order[votes]': 'DESC',
+      status: 1
+    })
+    .then(({data}) => {
+      res.render('index', {
+        user: req.user,
+        requests: data
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+}
+
+function send404(req, res) {
+  res.status(404).render('404');
+}
 
 module.exports = router;
