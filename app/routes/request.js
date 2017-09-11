344725923417854c9ec7239525c271366eaeba25
rijkvanzanten <rijkvanzanten@me.com>
2017-09-11 16:45:52 -0400

    Render single request to screen

--- app/routes/request.js
+++ app/routes/request.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
+const timeago = require('timeago.js');
+const marked = require('marked');
 
 const directus = require('../directus');
 
+const parseRequestData = request => ({
+  title: request.title,
+  votes: request.votes_offset + request.voted_on_by.data.length,
+  username: request.user.data.username,
+  closed: Boolean(request.closed),
+  date: new Date(...request.date.split('-')),
+  date_relative: timeago().format(new Date(...request.date.split('-'))),
+  comments: request.comments.data.map(comment => ({
+    content: marked(comment.content),
+    username: comment.user_id,
+    date_relative: timeago().format(new Date(...comment.date.split('-'))),
+  }))
+});
+
 const renderRequest = (req, res) => {
   directus.getItem('requests', req.params.id)
-    .then(({data: request}) => res.render('request', {request}))
+    // .then(({data: request}) => request)
+    .then(({data: request}) => parseRequestData(request))
+    .then(request => res.render('request', {request}))
     .catch(err => {
       console.error(err);
       res.status(500).end();
