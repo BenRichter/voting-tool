05f13b99e4d9eeac1730bf9893b1be723a615458
rijkvanzanten <rijkvanzanten@me.com>
2017-09-12 10:42:01 -0400

    Add route for adding comments

---
+++ app/routes/comment.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const directus = require('../directus');
+
+const placeComment = (req, res) => {
+  const {request_id, content} = req.body;
+  const userID = req.user.id;
+
+  // If user isn't logged in, redirect back
+  if (!req.user) return res.redirect('/r/' + request_id);
+
+  directus.createItem('comments', {
+    user_id: userID,
+    request_id,
+    content,
+    date: 'NOW()',
+    active: 1
+  })
+    .then(() => res.redirect('/r/' + request_id))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+};
+
+module.exports = router
+  .post('/', placeComment);
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const auth = require('./auth');
 const request = require('./request');
 const vote = require('./vote');
+const comment = require('./comment');
 
 const directus = require('../directus');
 
@@ -18,5 +19,6 @@ const renderHomepage = (req, res) => {
 module.exports = router
   .use('/auth', auth)
   .use('/vote', vote)
+  .use('/comment', comment)
   .get('/', renderHomepage)
   .use('/r/', request);
