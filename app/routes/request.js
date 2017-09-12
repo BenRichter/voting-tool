c74c0e2a479bd4b34bae55d437a1efc4a6af8771
rijkvanzanten <rijkvanzanten@me.com>
2017-09-12 11:08:27 -0400

    Add 'edit' mode to request

--- app/routes/request.js
+++ app/routes/request.js
@@ -9,14 +9,23 @@ const parseRequestData = request => ({
   title: request.title,
   votes: request.votes_offset + request.voted_on_by.data.length,
   username: request.user.data.username,
+  user_id: request.user.data.id,
   closed: Boolean(request.closed),
   date: new Date(...request.date.split('-')),
   date_relative: timeago().format(new Date(...request.date.split('-'))),
   comments: request.comments.data
     .filter(comment => comment.active === 1)
     .map(comment => ({
-      content: marked(comment.content),
+      content_parsed: marked(comment.content),
+      content: comment.content,
+
+      // Bug: directus/directus#1806
+      // username: comment.user.data.username,
+      // user_id: comment.user.data.id,
+
       username: comment.user_id,
+      user_id: comment.user_id,
+
       date_relative: timeago().format(new Date(...comment.date.split('-'))),
     }))
 });
@@ -25,7 +34,14 @@ const renderRequest = (req, res) => {
   directus.getItem('requests', req.params.id)
     // .then(({data: request}) => request)
     .then(({data: request}) => parseRequestData(request))
-    .then(request => res.render('request', {request}))
+    .then(request => {
+      const locals = {
+        request,
+        user: req.user
+      };
+      if (req.query.edit == 1) locals.editMode = true;
+      res.render('request', locals)
+    })
     .catch(err => {
       console.error(err);
       res.status(500).end();
