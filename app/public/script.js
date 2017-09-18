7a9c9c990a8316c42badca653ff8b45bbddb504c
rijkvanzanten <rijkvanzanten@me.com>
2017-09-18 14:34:36 -0400

    Only show button focus styles for keyboard users

--- app/public/script.js
+++ app/public/script.js
@@ -1,5 +1,6 @@
 (function() {
   // Change height of textinput in new comment input based on value
+  // ----------------------
   var commentTextarea = document.querySelector('.write-comment textarea');
 
   if (commentTextarea) {
@@ -13,4 +14,23 @@
     onInput.call(commentTextarea);
   }
 
+
+  // Only show focus styles to keyboard users
+  function handleFirstTab(e) {
+    if (e.code === 'Tab') {
+      document.body.classList.remove('using-mouse');
+
+      window.removeEventListener('keydown', handleFirstTab);
+      window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+  }
+
+  function handleMouseDownOnce() {
+    document.body.classList.add('using-mouse');
+
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+  }
+
+  window.addEventListener('mousedown', handleMouseDownOnce);
 }())
